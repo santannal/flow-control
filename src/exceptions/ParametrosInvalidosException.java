4c63ab2608d4029842f3a57b05ba215db717d3b4
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-10-03 14:24:29 -0300

    first commit

---
+++ src/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+}
